b1de15ad0d9e00d4a40af30a70dc25158da139a1
aniket <aniketpatil108108@gmail.com>
2025-07-17 20:48:55 +0530

    new work

---
+++ src/main/java/com/aniket/cicd/CicdwithspringApplication.java
@@ -0,0 +1,13 @@
+package com.aniket.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdwithspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdwithspringApplication.class, args);
+	}
+
+}
